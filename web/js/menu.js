d1c4794553699e9d656f5424d222a62eacc1f17b
Li-Zhenghao-Harbin <a15846593095@126.com>
2023-04-20 22:36:40 +0100

    unified menu

---
+++ web/js/menu.js
@@ -0,0 +1,3 @@
+function setMenu() {
+    document.getElementById("menu").innerHTML = "<p class=title>在线课程注册系统</p><p class=option_left>免费课程</p><p class=option_left>付费课程</p><p class=option_right>user_name</p><p class=option_right>我的订单</p><p class=option_right>我的课程</p>";
+}
