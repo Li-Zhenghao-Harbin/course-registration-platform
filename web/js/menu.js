b38859c6eb622f8ff041366b86806817fa836999
Li-Zhenghao-Harbin <a15846593095@126.com>
2023-04-23 21:52:51 +0100

    platform_tch.html

--- web/js/menu.js
+++ web/js/menu.js
@@ -1,3 +1,7 @@
-function setMenu() {
+function setStuMenu() {
     document.getElementById("menu").innerHTML = "<p class=title>在线课程注册系统</p><p class=option_left>免费课程</p><p class=option_left>付费课程</p><p class=option_right id=me>user_name</p><p class=option_right>我的订单</p><p class=option_right>我的课程</p>";
+}
+
+function setTchMenu() {
+    document.getElementById("menu").innerHTML = "<p class=title>在线课程注册系统（教师端）</p><p class=option_right id=me>user_name</p>"
 }
